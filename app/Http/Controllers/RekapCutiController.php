2bac8c6cfafd9c4a01fc27987c758c83f4d2feb7
EgoWinasis <egowinasis22@gmail.com>
2025-05-12 20:03:33 +0700

    export rekap cuti

--- app/Http/Controllers/RekapCutiController.php
+++ app/Http/Controllers/RekapCutiController.php
@@ -72,7 +72,16 @@ class RekapCutiController extends Controller
     
     public function exportPDF(Request $request)
     {
-        // Build Cuti query
+       // Get the logged-in user
+       $user = Auth::user();
+    
+       // Get filters from request
+       $month = $request->get('month', Carbon::now()->month);
+       $startDate = $request->get('start_date');
+       $endDate = $request->get('end_date');
+       $selectedUserId = $request->get('user_id');
+
+       
         if ($user->role == 'user') {
             $cutiQuery = Cuti::where('user_id', $user->id);
         } else {
